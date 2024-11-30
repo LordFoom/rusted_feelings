68974304470f26ec7f1ed98cab2887ac201f65dd
lordfoom <lordfoom@gmail.com>
2024-11-30 21:39:19 +0200

    Refactor and test stub
    Break up db methods to facilitate testing and future comprehension.

--- src/db.rs
+++ src/db.rs
@@ -1,19 +1,33 @@
-use anyhow::Result;
+use anyhow::{Ok, Result};
 use rusqlite::Connection;
 
 struct AppDb {
     pub path: String,
     pub conn: Connection,
 }
+
+impl AppDb {
+    pub fn new(path: String, conn: Connection) -> Self {
+        Self { path, conn }
+    }
+}
 pub fn init_db(path: &str) -> Result<AppDb> {
-    let sql = "CREATE TABLE IF NOT EXISTS mood_journal (primary key id, score REAL)";
     let conn = Connection::open(path)?;
-    conn.execute(sql, [])?;
-    let db = AppDb {
-        path: path.to_string(),
-        conn,
-    };
+    let db = AppDb::new(path.to_string(), conn);
+    init_db_tables(&db.conn);
     Ok(db)
 }
 
-mod test {}
+pub fn init_db_tables(conn: &Connection) -> Result<()> {
+    let sql = "CREATE TABLE IF NOT EXISTS mood_journal (primary key id, score REAL)";
+    conn.execute(sql, [])?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_init_db() {
+        let test_db_file_path = ".test.sqlite.db";
+    }
+}
