64573526d28df06f45228b58af09dd954a139ccd
lordfoom <lordfoom@gmail.com>
2024-12-07 21:43:18 +0200

    Checking sqlite table check
    Testing continues

--- src/db.rs
+++ src/db.rs
@@ -19,15 +19,29 @@ pub fn init_db(path: &str) -> Result<AppDb> {
 }
 
 pub fn init_db_tables(conn: &Connection) -> Result<()> {
-    let sql = "CREATE TABLE IF NOT EXISTS mood_journal (primary key id, score REAL)";
-    conn.execute(sql, [])?;
+    let score_tbl_sql = "CREATE TABLE IF NOT EXISTS score (primary key id, score REAL)";
+    conn.execute(score_tbl_sql, [])?;
+    let tag_tbl_sql = "CREATE TABLE IF NOT EXISTS tag (primary key id, name TEXT)";
+    conn.execute(tag_tbl_sql, [])?;
+
     Ok(())
 }
 
 #[cfg(test)]
 mod test {
+    use rusqlite::Connection;
+
+    use super::init_db_tables;
+
     #[test]
     fn test_init_db() {
-        let test_db_file_path = ".test.sqlite.db";
+        let conn = Connection::open_in_memory().unwrap();
+        init_db_tables(&conn).unwrap();
+        //somehow check that there iss a db
+        let table_query = r#"SELECT EXISTS ( 
+            SELETCT 1 FROM sqlite_master where type='table' and name=?)";
+        conn.query_row(sql, params, f)"#;
     }
+
+    fn get_test_conn() {}
 }
