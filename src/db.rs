7391e34b3c72f4f99f292b9ae68c46463b74539e
lordfoom <lordfoom@gmail.com>
2024-12-31 17:01:13 +0200

    Refactoring; Insert statement

--- src/db.rs
+++ src/db.rs
@@ -62,23 +62,20 @@ pub fn add_mood_score(
     db: &AppDb,
 ) -> Result<String> {
     //get the mood to ensure it exists
-    let mut stmt = db
-        .conn
-        .prepare("SELECT COUNT(*) FROM mood WHERE name = ?")?;
+    let mut stmt = db.conn.prepare("SELECT id FROM mood WHERE name = ?")?;
     let mut rows = stmt.query([])?;
-    let count: usize = if let Some(row) = rows.next()? {
+    let mood_id: usize = if let Some(row) = rows.next()? {
         row.get(0)?
     } else {
-        return Err(eyre!("No row returned from mood COUNT query??"));
-    };
-
-    if count == 0 {
         //TODO think about whether autocreating this is the way to go? I worry about typos...
         //TODO add a flag that allows autocreation of the mood if it is passed in
         return Err(eyre!(
             "The mood {mood} was not found, you will need to add it first"
         ));
-    }
+    };
+
+    let mut insert_score_sql = ;
+    db.conn.execute("INSERT INTO score(mood_id, score) VALUES(?,?)", [mood_id, score])?;
 
     let ok = String::from("OK");
     Ok(ok)
