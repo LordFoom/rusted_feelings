8e99b10e849e6d5fb2a535b4819fecf8acd11495
Timothy Spring <foom@amazon.com>
2025-02-12 18:44:19 +0200

    Up version number
    Can literally use it for my intended purpose

--- src/args.rs
+++ src/args.rs
@@ -4,7 +4,7 @@ use rust_decimal::Decimal;
 ///Representation of the command line arguments
 #[derive(Parser, Debug)]
 #[command(
-    version = "0.1",
+    version = "1.0",
     about = "Mood tracker",
     long_about = "Keep track of your moods with this cli app. Give it a score and maybe tag it with feelings"
 )]
